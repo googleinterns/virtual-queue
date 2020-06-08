41d99ca4a1eff73b235e31a8cb9d5bfef893fbdf
Shradha <shradha.sgl@gmail.com>
2020-06-08 17:57:59 +0530

    Redirection of marker + display of places started

--- frontend/src/views/Map.vue
+++ frontend/src/views/Map.vue
@@ -3,14 +3,22 @@
     <div>
       <label>
         <gmap-autocomplete
+          style="width:50%;"
           @place_changed="setPlace">
-        </gmap-autocomplete>
+        </gmap-autocomplete
+        >
         <button @click="addMarker">Add</button>
       </label>
       <br/>
 
     </div>
     <br>
+
+    <ul id="example-1">
+  <li v-for="mark in markers" :key="mark.location">
+    {{ mark.location }}
+  </li>
+</ul>
     <gmap-map
       :center="center"
       :zoom="12"
@@ -20,31 +28,78 @@
         :key="index"
         v-for="(m, index) in markers"
         :position="m.position"
-        @click="center=m.position"
+        @click="Queue"
       ></gmap-marker>
     </gmap-map>
   </div>
 </template>
 
 <script>
+import firebase from "firebase";
 export default {
   name: "Map",
   data() {
     return {
       // default to Montreal to keep it simple
       // change this to whatever makes sense
+
       center: { lat: 28.7041, lng: 78.1025 },
-      markers: [{position: {lat: 28.5479, lng: 77.2031 }}],
+      markers: [
+        {
+          location: "Hotel 1",
+          position: {lat: 28.5379, lng: 77.1970},
+          queue_id: "1",
+        },
+        {
+          location: "Hotel 2",
+          position: {lat: 28.5473, lng: 77.2034}
+        }
+      ],
+      // markers: [{position: {lat: 28.5479, lng: 77.2031 }, location:"Hotel Palace", queueid:"1"}],
       places: [],
-      currentPlace: null
+      currentPlace: null,
     };
   },
 
   mounted() {
     this.geolocate();
+    firebase.database().ref().child("Store").once("value", function(snap){
+      // this.markers = null
+      snap.forEach(function(childSnap){
+        var store = childSnap.val();
+        console.log(store.StoreName, store.lat, store.lng);
+        // this.markers.push({ 
+        //   location: store.StoreName,
+        //   position: {lat: store.lat, lng: store.lng}
+        // });
+      });
+      
+    });
+
   },
 
+  // created(){
+
+  //   firebase.database().ref().child("Store").once("value", function(snap){
+  //     // this.markers = null
+  //     snap.forEach(function(childSnap){
+  //       var store = childSnap.val();
+  //       console.log(store.StoreName, store.lat, store.lng);
+  //       this.markers.push({ 
+  //         location: store.StoreName,
+  //         position: {lat: store.lat, lng: store.lng}
+  //       });
+  //     });
+      
+  //   });
+
+  // },
+
   methods: {
+
+    Queue(){
+      this.$router.replace("sign-up");
+    },
     // receives a place object via the autocomplete component
     setPlace(place) {
       this.currentPlace = place;
