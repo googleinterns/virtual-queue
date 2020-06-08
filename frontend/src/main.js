dea613f7d8105b65c8bc229876ed5c6f6892cf00
Shradha <shradha.sgl@gmail.com>
2020-06-08 21:53:20 +0530

    Map page done + UI styling

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,12 +3,12 @@ import firebase from 'firebase'
 import App from './App.vue'
 import router from './router'
 import * as VueGoogleMaps from "vue2-google-maps";
+import './../node_modules/bulma/css/bulma.css';
 
 Vue.config.productionTip = false
 let app = '';
 // Your web app's Firebase configuration
 const firebaseConfig = {
-<<<<<<< HEAD
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: process.env.VUE_APP_FIREBASE_PROJECT_ID + '.firebaseapp.com',
   databaseURL: "https://" + process.env.VUE_APP_FIREBASE_PROJECT_ID + '.firebaseio.com',
@@ -17,15 +17,6 @@ const firebaseConfig = {
   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.VUE_APP_FIREBASE_APPID,
   measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
-=======
-  apiKey: "AIzaSyC6ybwkhLl8KqQHHOwEr6HMaetCRM27_vo",
-  authDomain: "database-queue.firebaseapp.com",
-  databaseURL: "https://database-queue.firebaseio.com",
-  projectId: "database-queue",
-  storageBucket: "database-queue.appspot.com",
-  messagingSenderId: "404151767225",
-  appId: "1:404151767225:web:dc2da4bffb8c8d973d4b68"
->>>>>>> 2aed568... Redirection of marker + display of places started
 };
 // Initialize Firebase
 
