e980138515d56fd8045b3c319c1106043d3de5d6
Garima Jain <garimaj012@gmail.com>
2020-06-15 22:07:43 +0000

    Added Google SignIn option

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,42 +1,45 @@
 <template>
   <div id="app">
+    <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+      <div class="navbar-brand">
+        <a
+          role="button"
+          class="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
 
-
-<nav class="navbar is-dark" role="navigation" aria-label="main navigation">
-  <div class="navbar-brand">
-    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-      <span aria-hidden="true"></span>
-    </a>
-  </div>
-
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-      <a class="navbar-item">
-        Home
-      </a>
-
-      <a class="navbar-item">
-        Queues
-      </a>
-
-    </div>
-
-    <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button is-light">
-            Sign up
+      <div id="navbarBasicExample" class="navbar-menu">
+        <div class="navbar-start">
+          <a class="navbar-item">
+            Home
           </a>
-          <a class="button is-light">
-            Log in
+
+          <a class="navbar-item">
+            Queues
           </a>
         </div>
+
+        <div class="navbar-end">
+          <div class="navbar-item">
+            <div class="buttons">
+              <a class="button is-light">
+                Sign up
+              </a>
+              <a class="button is-light">
+                Log in
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-</nav>
+    </nav>
     <router-view />
   </div>
 </template>
@@ -63,16 +66,16 @@
   color: #42b983;
 }
 
-.navbar{
+.navbar {
   margin-bottom: 5%;
   padding: px;
 }
 
-.navbar-brand{
+.navbar-brand {
   margin-left: 5%;
 }
 
-.navbar-end{
+.navbar-end {
   margin-right: 5%;
 }
 </style>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -5,7 +5,12 @@
     <br />
     <input type="password" v-model="password" placeholder="Password" />
     <br />
+    <br />
     <button @click="login">Connect</button>
+    <p>OR</p>
+    <button @click="googleSignIn">SignIn with Google</button>
+    <br />
+    <br />
     <p>
       You don't have an account? You can
       <router-link to="/sign-up">create one</router-link>
@@ -36,6 +41,21 @@ export default {
             alert("Oops," + err.message);
           }
         );
+    },
+    googleSignIn: function() {
+      var provider = new firebase.auth.GoogleAuthProvider();
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then(result => {
+          console.log(result);
+          console.log("Success");
+          this.$router.replace("home");
+        })
+        .catch(function(error) {
+          console.log(error);
+          console.log("UnSuccessful");
+        });
     }
   }
 };
