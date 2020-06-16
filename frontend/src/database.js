a2619e8a429b4bd61abc0ff6730b18a6e7c99613
= <sindhugunturi@google.com>
2020-06-16 15:48:48 +0530

    Added review changes

--- frontend/src/database.js
+++ frontend/src/database.js
@@ -1,25 +1,75 @@
 import firebase from "firebase";
 
-export function getQueuePosition(storeId, userId, callBack){
-    //Pass a callBack function as an argument while calling this function, the callBack function has queuePosition as an argument
-    //If userId is null, function returns queueLength
-    let dbRef = firebase.database().ref();
-    dbRef.child(this.getUserPath(storeId)).once("value", snap => {
-        var queuePosition = 1;
-        snap.forEach(function(childSnap){
-            if(userId==childSnap.val().UserID){
-                return true;
-            }
-            queuePosition++;
-        });
-        callBack(queuePosition);
-    });
-}
+const STORE_ID_STRING = "StoreID";
+const USER_ID_STRING = "UserID";
 
 export const database_call = {
+
+    //return userId of currently loggged in user
+    getUserId: function(){
+        return firebase.auth().currentUser.uid;
+    },
+
+    //Returns whether user with userId is enrolled in store with storeId
+    isUserInQueue: function(storeId, userId, callBack){
+        let dbRef = firebase.database().ref();
+        dbRef.child(this.getStorePath(userId)).orderByChild(STORE_ID_STRING).equalTo(storeId).once("value", snap =>{
+            if(snap.exists()){
+                callBack(true);
+            } 
+            else{
+                callBack(false);
+            } 
+        });
+    },
+
+    //Add user to store and returns: currentUserKey, currentStoreKey, error
+    addToQueue: function(storeId, userId, callBack){
+        let dbRef = firebase.database().ref();
+
+        //Enter User to UsersInQueue
+        var currentUserRef = dbRef.child(this.getUserPath(storeId)).push();
+        var currentUserKey = currentUserRef.key;
+
+        //Enter Store to SubscribedStoreID
+        var currentStoreRef = dbRef.child(this.getStorePath(userId)).push();
+        var currentStoreKey = currentStoreRef.key;
+
+        var updateQueue = {};
+        updateQueue[this.getUserPath(storeId)+"/"+currentUserKey] = {
+            UserID : userId
+        };
+        updateQueue[this.getStorePath(userId)+"/"+currentStoreKey] = {
+            StoreID : storeId
+        };
+
+        dbRef.update(updateQueue, function(error){
+            callBack(currentStoreKey, currentUserKey, error);
+        });
+    },
+
+    removeFromQueue: function(storeId, userId, storeKey, userKey, callBack){
+        let dbRef = firebase.database().ref();
+
+        var updateQueue = {};
+        //Remove User from UsersInQueue
+        updateQueue[this.getUserPath(storeId)+"/"+userKey] = {
+            UserID : null
+        };
+
+        //Remove Store from SubscribedStoreID
+        updateQueue[this.getStorePath(userId)+"/"+storeKey] = {
+            StoreID : null
+        };
+
+        dbRef.update(updateQueue, function(error){
+            callBack(error);
+        });
+    },
+
+    //Pass a callBack function as an argument while calling this function, the callBack function has queuePosition as an argument
+    //If userId is null, function returns queueLength
     getQueuePosition: function(storeId, userId, callBack){
-        //Pass a callBack function as an argument while calling this function, the callBack function has queuePosition as an argument
-        //If userId is null, function returns queueLength
         let dbRef = firebase.database().ref();
         dbRef.child(this.getUserPath(storeId)).once("value", snap => {
             var queuePosition = 1;
@@ -32,10 +82,11 @@ export const database_call = {
             callBack(queuePosition);
         });
     },
+
+    //Pass a callBack function as an argument while calling this function, the callBack function has queuePosition as an argument
     getCurrentUserKey: function(storeId, userId, callBack){
-        //Pass a callBack function as an argument while calling this function, the callBack function has queuePosition as an argument
         let dbRef = firebase.database().ref();
-        dbRef.child(this.getUserPath(storeId)).orderByChild("UserID").equalTo(userId).once("value", snap => {
+        dbRef.child(this.getUserPath(storeId)).orderByChild(USER_ID_STRING).equalTo(userId).once("value", snap => {
             var currentUserKey = null;
             if(snap.exists()){
                 currentUserKey = Object.keys(snap.val())[0];
@@ -43,10 +94,11 @@ export const database_call = {
             callBack(currentUserKey);
         });
     },
+
+    //Pass a callBack function as an argument while calling this function, the callBack function has queuePosition as an argument
     getCurrentStoreKey: function(storeId, userId, callBack){
-        //Pass a callBack function as an argument while calling this function, the callBack function has queuePosition as an argument
         let dbRef = firebase.database().ref();
-        dbRef.child(this.getStorePath(userId)).orderByChild("StoreID").equalTo(storeId).once("value", snap =>{
+        dbRef.child(this.getStorePath(userId)).orderByChild(STORE_ID_STRING).equalTo(storeId).once("value", snap =>{
             var currentStoreKey = null;
             if(snap.exists()){
                 currentStoreKey = Object.keys(snap.val())[0];
@@ -54,13 +106,28 @@ export const database_call = {
             callBack(currentStoreKey);
         });
     },
+
+    //Get path to SubscribedStoreID child of user with userId
     getStorePath: function(userId){
         return `User/${userId}/SubscribedStoreID`;
     },
+
+    //Get path to UsersInQueue of store with storeId
     getUserPath: function(storeId){
         return `Store/${storeId}/UsersInQueue`;
     },
-    printHello: function(){
-        console.log("Hello!");
+    
+    //Set listener on UsersinQueue and listen for addition to queue
+    setQueueIncListener: function(storeId, callBack){
+        let dbRef = firebase.database().ref();
+
+        dbRef.child(this.getUserPath(storeId)).on("child_added", callBack);
+    },
+
+    //Set listener on UsersinQueue and listen for deletion from queue
+    setQueueDecListener: function(storeId, callBack){
+        let dbRef = firebase.database().ref();
+
+        dbRef.child(this.getUserPath(storeId)).on("child_removed", callBack);
     }
 }
