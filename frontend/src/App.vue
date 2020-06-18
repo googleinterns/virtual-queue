4459d9b3e6753e20bc8ef8b0ddede37b99b1e731
shradhasehgal <shradhasehgal@google.com>
2020-06-18 17:01:02 +0530

    Alignment fixes + webapp version completed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -91,6 +91,8 @@ export default {
 </script>
 
 <style>
+/* @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@600&display=swap'); */
+
 #app {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
