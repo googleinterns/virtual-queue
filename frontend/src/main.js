1d8d89de5d7e3539a8036e370a881c26aed4c34a
shradhasehgal <shradhasehgal@google.com>
2020-06-18 11:12:21 +0530

    Icons added and page styled

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,15 @@ import "./../node_modules/bulma/css/bulma.css";
 import HighchartsVue from "highcharts-vue";
 import Highcharts from "highcharts";
 import Annotations from "highcharts/modules/annotations.js";
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { dom } from '@fortawesome/fontawesome-svg-core'
+
+dom.watch()
+
+library.add(fas)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.use(HighchartsVue);
 Annotations(Highcharts);
