b701867bfdd832f73e6103f159b3ba50914db74d
shradhasehgal <shradhasehgal@google.com>
2020-06-20 02:07:52 +0530

    Added code explanation comments

--- frontend/src/populateStoreInfo.js
+++ frontend/src/populateStoreInfo.js
@@ -22,11 +22,14 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
+// USER_ID that will be assigned the stores ownership
 const USER_ID = "2L2GVG7rIPZojeSEixUWDdsu2Eh1";
 
 rl.question("Please enter your location ", function(location) {
   let shops = ["liquor", "food", "grocery", "pizza", "restaurant", "medicine"];
+  // Iterates over various types of common shops
   for (var i = 0; i < shops.length; i++) {
+    // Requests Places Search API for relevant stores near input location
     axios
       .get(
         "https://maps.googleapis.com/maps/api/place/textsearch/json?query=" +
@@ -39,6 +42,7 @@ rl.question("Please enter your location ", function(location) {
       .then((response) => {
         for (var j = 0; j < response.data.results.length; j++) {
           let place_id = response.data.results[j].place_id;
+          // Iterates over each place ID and request Places Details API for address, phone, opening hrs & rating
           axios
             .get(
               "https://maps.googleapis.com/maps/api/place/details/json?place_id=" +
@@ -51,12 +55,16 @@ rl.question("Please enter your location ", function(location) {
               console.log(storeDetails.name);
               let values = {
                 IsEnabled: true,
+                // Assigns a random value between 5-15 (both inclusive) to the serve time of a store
                 AvgServeTime: Math.floor(Math.random() * 10) + 5,
+                // Assigns a value between 1-51 (both inclusive) to the current token of store
                 CurrentToken: Math.floor(Math.random() * 50) + 1,
                 StoreName: storeDetails.name,
+                // Initializes queue length as 0 as no users are enrolled yet
                 QueueLength: 0,
               };
 
+              // Checks for presense of attributes in Places Details API results and assigns values accordingly
               if (storeDetails.formatted_phone_number)
                 values.Phone = storeDetails.formatted_phone_number;
 
@@ -69,12 +77,13 @@ rl.question("Please enter your location ", function(location) {
                 storeDetails.opening_hours &&
                 storeDetails.opening_hours.periods
               )
-                values.OperationalHours = storeDetails.opening_hours.periods;
-
+                values.OperatsionalHours = storeDetails.opening_hours.periods;
+              // Adds all store details to DB with place ID 
               firebase
                 .database()
                 .ref("Store/" + place_id)
                 .set(values);
+              // Assigns USER_ID the ownership of the store in DB
               firebase
                 .database()
                 .ref("User/" + USER_ID + "/OwnedStoreID/" + place_id)
