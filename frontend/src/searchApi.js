da8f89067eebe7023548480c94dfdf579cf517a9
shradhasehgal <shradhasehgal@google.com>
2020-06-22 19:36:32 +0530

    Added Search API file

---
+++ frontend/src/searchApi.js
@@ -0,0 +1,35 @@
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
+
+export const search_api = {
+  calculateTravelTime: function(place_id) {
+    let center = this.$userLocation;
+    let distanceParams = {
+      origins: center.lat + "," + center.lng,
+      destinations: "place_id:" + place_id,
+      departure_time: "now",
+      key: process.env.VUE_APP_DISTANCE_API_KEY,
+    };
+
+    return axios
+      .get(process.env.VUE_APP_DISTANCE_URL, {
+        params: distanceParams,
+      })
+      .then((response) => {
+        // returning the data here allows the caller to get it through another .then(...)
+        return response;
+      });
+  },
+
+  geolocate: function() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      let userLocation = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude,
+      };
+      return userLocation;
+    });
+  },
+};
