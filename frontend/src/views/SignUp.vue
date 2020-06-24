f30144f0ef44d67d2f7f04b5dadfef91a292c18c
shradhasehgal <shradhasehgal@google.com>
2020-06-25 01:18:59 +0530

    Incorporated logo

--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -1,49 +1,47 @@
 <template>
   <div>
-    <div>
-      <img src="../assets/logo-name.png" class="logo-name" />
-      <h1 class="title is-3">Sign Up</h1>
-      <span v-html="signupContent"></span>
-      <div class="inner box">
-        <div class="field">
-          <p class="control has-icons-left">
-            <input
-              class="input"
-              v-model="email"
-              type="email"
-              placeholder="Email"
-            />
-            <span class="icon is-small is-left">
-              <i class="fas fa-envelope"></i>
-            </span>
-          </p>
-        </div>
-        <div class="field">
-          <p class="control has-icons-left">
-            <input
-              class="input"
-              v-model="password"
-              type="password"
-              placeholder="Password"
-            />
-            <span class="icon is-small is-left">
-              <i class="fas fa-lock"></i>
-            </span>
-          </p>
-        </div>
-        <div class="field">
-          <p class="control">
-            <button class="button is-info full" @click="signup">
-              Register
-            </button>
-          </p>
-        </div>
+    <img src="../assets/logo.png" class="logo" />
+    <h1 class="title is-3">Sign Up</h1>
+    <span v-html="signupContent"></span>
+    <div class="inner box">
+      <div class="field">
+        <p class="control has-icons-left">
+          <input
+            class="input"
+            v-model="email"
+            type="email"
+            placeholder="Email"
+          />
+          <span class="icon is-small is-left">
+            <i class="fas fa-envelope"></i>
+          </span>
+        </p>
       </div>
-      <span>
-        Already have an account?
-        <router-link to="/login">Login</router-link>
-      </span>
+      <div class="field">
+        <p class="control has-icons-left">
+          <input
+            class="input"
+            v-model="password"
+            type="password"
+            placeholder="Password"
+          />
+          <span class="icon is-small is-left">
+            <i class="fas fa-lock"></i>
+          </span>
+        </p>
+      </div>
+      <div class="field">
+        <p class="control">
+          <button class="button is-info full" @click="signup">
+            Register
+          </button>
+        </p>
+      </div>
     </div>
+    <span>
+      Already have an account?
+      <router-link to="/login">Login</router-link>
+    </span>
   </div>
 </template>
 
